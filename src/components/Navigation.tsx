fdfe101b369daab70c6305a1e51e0c72c5703f24
DarkSky3309 <lust3309@gmail.com>
2023-03-09 17:26:26 +0300

    add alert on decline permission

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,29 +10,27 @@ interface navigationType {
 
 const Navigation: FC<navigationType> = ({setCity, setUnits}) => {
     const [searchCity, setSearchCity] = useState('');
-    const [currentLocation, setCurrentLocation]:[{latitude: number, longitude: number} | undefined, Dispatch<SetStateAction<{latitude: number, longitude: number}| undefined>> ] = useState();
+    const [currentLocation, setCurrentLocation]: [{ latitude: number, longitude: number } | undefined, Dispatch<SetStateAction<{ latitude: number, longitude: number } | undefined>>] = useState();
 
     const lookForCity = () => {
         setCity(searchCity);
         setSearchCity("")
     }
-    function enablePermissions () {
-        navigator.permissions.query({name: "geolocation"}).then((result) => {
-            if (result.state === "granted")
-                console.log("granted")
-            else if (result.state === "prompt")
-                console.log("prompt")
-            console.log("den")
-        })
+
+    function showError(e) {
+        alert("Please give permission to your geolocation")
     }
 
+
     const fetchCity = async () => {
-        let data:any
-        if (currentLocation){
+        let data: any
+        if (currentLocation) {
             try {
                 data = await getCity({lat: currentLocation.latitude, lon: currentLocation.longitude})
                 setCity(data[0].name)
-            } catch (e) {console.error(e)}
+            } catch (e) {
+                console.error(e)
+            }
         }
     }
     useEffect(() => {
@@ -41,41 +39,48 @@ const Navigation: FC<navigationType> = ({setCity, setUnits}) => {
 
 
     const getLocation = async () => {
-        enablePermissions();
         navigator.geolocation.getCurrentPosition((position) => {
             const {latitude, longitude} = position.coords;
             setCurrentLocation({latitude, longitude})
         })
+        navigator.permissions.query({name: "geolocation"}).then((result) => {
+            if (result.state === "denied") {
+                showError(result.state);
+            }
+        })
     }
 
-    return (
-        <header className={"w-11/12 mx-auto flex justify-between text-white py-6 text-2xl"}>
-            <div className={"flex justify-between max-w-lg w-full"}>
-                <span onClick={() => setCity(CITY.kyiv)} className={"cursor-pointer"}>Kyiv</span>
-                <span onClick={() => setCity(CITY.kharkiv)} className={"cursor-pointer"}>Kharkiv</span>
-                <span onClick={() => setCity(CITY.odesa)} className={"cursor-pointer"}>Odesa</span>
-                <span onClick={() => setCity(CITY.lviv)} className={"cursor-pointer"}>Lviv</span>
-                <span onClick={() => setCity(CITY.dnepro)} className={"cursor-pointer"}>Dnepr</span>
+return (
+    <header className={"w-11/12 mx-auto flex justify-between text-white py-6 text-2xl"}>
+        <div className={"flex justify-between max-w-lg w-full"}>
+            <span onClick={() => setCity(CITY.kyiv)} className={"cursor-pointer"}>Kyiv</span>
+            <span onClick={() => setCity(CITY.kharkiv)} className={"cursor-pointer"}>Kharkiv</span>
+            <span onClick={() => setCity(CITY.odesa)} className={"cursor-pointer"}>Odesa</span>
+            <span onClick={() => setCity(CITY.lviv)} className={"cursor-pointer"}>Lviv</span>
+            <span onClick={() => setCity(CITY.dnepro)} className={"cursor-pointer"}>Dnepr</span>
+        </div>
+        <div className={"flex gap-5"}>
+            <div className={"relative"}>
+                <input value={searchCity} onKeyDown={(event) => {
+                    event.key === "Enter" ? lookForCity() : ""
+                }} onChange={(e) => setSearchCity(e.target.value.toString())} type="text" placeholder={"Search..."}
+                       className={"text-xl text-black py-1 px-2 rounded-3xl"}/>
+                <button type={"submit"} onClick={() => lookForCity()}
+                        className={"text-black bg-transparent border-none cursor-pointer inline-block absolute top-0 right-0 pr-2 hover:text-deep-cold duration-200"}>
+                    <i className="ri-search-line"></i></button>
             </div>
-            <div className={"flex gap-5"}>
-                <div className={"relative"}>
-                    <input value={searchCity} onKeyDown={(event) => {
-                        event.key === "Enter" ? lookForCity() : ""
-                    }} onChange={(e) => setSearchCity(e.target.value.toString())} type="text" placeholder={"Search..."}
-                           className={"text-xl text-black py-1 px-2 rounded-3xl"}/>
-                    <button type={"submit"} onClick={() => lookForCity()}
-                            className={"text-black bg-transparent border-none cursor-pointer inline-block absolute top-0 right-0 pr-2 hover:text-deep-cold duration-200"}>
-                        <i className="ri-search-line"></i></button>
-                </div>
-                <span onClick={ () => { getLocation()}}><i className="ri-map-pin-line"></i></span>
-                <div>
-                    <span className={"cursor-pointer"} onClick={() => setUnits(UNITS.metric)}>°C</span>
-                    <span> | </span>
-                    <span className={"cursor-pointer"} onClick={() => setUnits(UNITS.imperial)}>°F</span>
-                </div>
+            <span onClick={() => {
+                getLocation()
+            }}><i className="ri-map-pin-line"></i></span>
+            <div>
+                <span className={"cursor-pointer"} onClick={() => setUnits(UNITS.metric)}>°C</span>
+                <span> | </span>
+                <span className={"cursor-pointer"} onClick={() => setUnits(UNITS.imperial)}>°F</span>
             </div>
-        </header>
-    );
-};
+        </div>
+    </header>
+);
+}
+;
 
 export default Navigation;
